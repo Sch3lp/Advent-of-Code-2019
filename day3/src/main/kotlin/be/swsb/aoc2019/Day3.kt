fd2f51328fe3ed39515ae012da6c10c26aa31a71
sch3lp <sch3lp@gmail.com>
2019-12-08 17:25:40 +0100

    until: A helper functions on Position

--- day3/src/main/kotlin/be/swsb/aoc2019/Day3.kt
+++ day3/src/main/kotlin/be/swsb/aoc2019/Day3.kt
@@ -1,5 +1,8 @@
 package be.swsb.aoc2019
 
+import be.swsb.aoc2019.Position.Companion.at
+import be.swsb.aoc2019.WireDirection.*
+
 // parse lines into wires ✅
 // parse U,D,L,R into wire direction commands ✅
 // keep positions for both wires
@@ -10,16 +13,16 @@ package be.swsb.aoc2019
 fun solve(wire1: List<String>, wire2: List<String>): Int = 0
 
 sealed class WireDirection(val steps: Int) {
-    data class Right(private val _steps: Int): WireDirection(_steps)
-    data class Left(private val _steps: Int): WireDirection(_steps)
-    data class Up(private val _steps: Int): WireDirection(_steps)
-    data class Down(private val _steps: Int): WireDirection(_steps)
+    data class Right(private val _steps: Int) : WireDirection(_steps)
+    data class Left(private val _steps: Int) : WireDirection(_steps)
+    data class Up(private val _steps: Int) : WireDirection(_steps)
+    data class Down(private val _steps: Int) : WireDirection(_steps)
 
-    companion object{
+    companion object {
         fun parseToWireDirection(direction: String): WireDirection {
-            val dir= direction[0]
+            val dir = direction[0]
             val steps = direction.substring(1)
-            return when(dir) {
+            return when (dir) {
                 'R' -> Right(steps.toInt())
                 'L' -> Left(steps.toInt())
                 'U' -> Up(steps.toInt())
@@ -30,8 +33,50 @@ sealed class WireDirection(val steps: Int) {
     }
 }
 
+fun applyWireDirections(directions: List<WireDirection>): List<Position> {
+    val visitedPositions = listOf(at(0, 0)).toMutableList()
+//    return directions.mapIndexedTo(visitedPositions) { i, it -> visitedPositions[i].pull(it)}
+    return visitedPositions
+}
+
+
 data class Position(val x: Int, val y: Int) {
+    fun pull(direction: WireDirection): List<Position> {
+        return when (direction) {
+            is Right -> this until at(this.x + direction.steps, this.y)
+            is Left -> this until at(this.x - direction.steps, this.y)
+            is Up -> this until at(this.x, this.y + direction.steps)
+            is Down -> this until at(this.x, this.y - direction.steps)
+        }
+    }
+
     companion object {
-        fun at(x: Int, y: Int): Position = Position(x,y)
+        fun at(x: Int, y: Int): Position = Position(x, y)
+    }
+}
+
+infix fun Position.until(position: Position): List<Position> {
+    return when {
+        this.x != position.x && this.y != position.y -> {
+            throw java.lang.IllegalArgumentException("Cannot get a range for unrelated axis")
+        }
+        this == position -> {
+            listOf(this)
+        }
+        this.x == position.x -> {
+            (this.y range position.y).map { at(this.x, it) }
+        }
+        this.y == position.y -> {
+            (this.x range position.x).map { at(it, this.y) }
+        }
+        else -> throw java.lang.IllegalArgumentException("I think we're not supposed to end up here")
+    }
+}
+
+infix fun Int.range(other: Int): Iterable<Int> {
+    return if (this < other) {
+        this..other
+    } else {
+        this.downTo(other)
     }
 }
--- day3/src/test/kotlin/be/swsb/aoc2019/Day3Test.kt
+++ day3/src/test/kotlin/be/swsb/aoc2019/Day3Test.kt
@@ -1,5 +1,6 @@
 package be.swsb.aoc2019
 
+import be.swsb.aoc2019.Position.Companion.at
 import be.swsb.aoc2019.WireDirection.*
 import be.swsb.aoc2019.WireDirection.Companion.parseToWireDirection
 import be.swsb.aoc2019.common.Common.asCsvLines
@@ -14,21 +15,61 @@ import org.junit.jupiter.api.TestInstance
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class Day3Test {
 
-    @Test
-    internal fun `Parsing into WireDirections`() {
-        assertThat(parseToWireDirection("R1")).isEqualTo(Right(1))
-        assertThat(parseToWireDirection("L20")).isEqualTo(Left(20))
-        assertThat(parseToWireDirection("U999")).isEqualTo(Up(999))
-        assertThat(parseToWireDirection("D42")).isEqualTo(Down(42))
-        assertThatExceptionOfType(IllegalArgumentException::class.java)
-                .isThrownBy { parseToWireDirection("F00") }
+    @Nested
+    inner class GeneralTests {
+        @Test
+        fun `Parsing into WireDirections`() {
+            assertThat(parseToWireDirection("R1")).isEqualTo(Right(1))
+            assertThat(parseToWireDirection("L20")).isEqualTo(Left(20))
+            assertThat(parseToWireDirection("U999")).isEqualTo(Up(999))
+            assertThat(parseToWireDirection("D42")).isEqualTo(Down(42))
+            assertThatExceptionOfType(IllegalArgumentException::class.java)
+                    .isThrownBy { parseToWireDirection("F00") }
+        }
+
+        @Test
+        fun `applying WireDirections returns a list of Positions`() {
+            val positionsVisited: List<Position> = applyWireDirections(listOf(Right(2), Up(3)))
+
+            assertThat(positionsVisited)
+                    .containsExactly(at(1, 0), at(2, 0), at(2, 1), at(2, 2), at(2, 3))
+        }
+
+        @Test
+        fun `pull in a WireDirection returns new Position`() {
+            assertThat(at(1, -1).pull(Right(1))).isEqualTo(at(2, -1))
+            assertThat(at(1, -1).pull(Left(4))).isEqualTo(at(-3, -1))
+            assertThat(at(1, -1).pull(Up(1))).isEqualTo(at(1, 0))
+            assertThat(at(1, -1).pull(Down(3))).isEqualTo(at(1, -4))
+        }
+    }
+
+    @Nested
+    inner class UntilTests {
+        @Test
+        fun `until with same Position returns a list of that one Position`() {
+            assertThat(at(0, 0) until at(0, 0)).containsExactly(at(0, 0))
+        }
+
+        @Test
+        fun `until returns list of all Positions between two Positions`() {
+            assertThat(at(0, 0) until at(1, 0)).containsExactly(at(0, 0), at(1, 0))
+            assertThat(at(1, 0) until at(0, 0)).containsExactly(at(1, 0), at(0, 0))
+            assertThat(at(0, 0) until at(3, 0)).containsExactly(at(0, 0), at(1, 0), at(2, 0), at(3, 0))
+        }
+
+        @Test
+        fun `until should explode when positions are on different axis`() {
+            assertThatExceptionOfType(java.lang.IllegalArgumentException::class.java)
+                    .isThrownBy { at(0, 0) until at(1, 1) }
+        }
     }
 
     @Nested
     inner class Exercise1 {
 
         @Test
-        internal fun `AoC example 1`() {
+        fun `AoC example 1`() {
             val result: Int = solve(
                     listOf("R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"),
                     listOf("U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83")
@@ -38,7 +79,7 @@ class Day3Test {
         }
 
         @Test
-        internal fun `AoC example 2`() {
+        fun `AoC example 2`() {
             val result: Int = solve(
                     listOf("R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"),
                     listOf("U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7")
@@ -50,7 +91,7 @@ class Day3Test {
         @Test
         fun `solve exercise 1`() {
             val wires = readLinesAs("actualInput.txt", ::asCsvLines)
-            val result = solve(wires[0],wires[1])
+            val result = solve(wires[0], wires[1])
             assertThat(result).isEqualTo(0)
         }
     }
